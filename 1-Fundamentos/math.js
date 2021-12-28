10d0acdb3bd5eed6ebe863145b033255bd9f86b0
Danilo Silva <danilo.silva@msn.com>
2021-12-27 23:24:32 -0300

    Fundamentos - Objeto Math

---
+++ 1-Fundamentos/math.js
@@ -0,0 +1,8 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+// Math.PI -> Constante com o valor de PI
+// Math.pow -> Realiza a potencialização
+
+console.log(area)
+console.log(typeof Math) // Tipo Object

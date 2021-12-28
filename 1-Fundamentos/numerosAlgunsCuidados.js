50cd9c8507fbcb48def90f2c3a9dd50a4c60d5bd
Danilo Silva <danilo.silva@msn.com>
2021-12-27 23:22:00 -0300

    Fundamentos - Cuidados com Tipos Numéricos

---
+++ 1-Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0); // Imprime Infinty
+console.log("10" / 2); // Realiza o cálculo aritmético
+console.log('3' + 2); // Em uma operação em Soma com String, ele realiza uma concatenação
+console.log('3' - 2); // Na subtração realiza uma cálculo aritmético
+console.log("Show!" * 2); // Se ele não entender como um número ele retorna NaN
+console.log(0.1 + 0.7); // Gera uma imprecisão
+// console.log(10.toString()); // Não consegue realizar a conversão diretamente em uma literal
+console.log((10.345).toFixed(2));

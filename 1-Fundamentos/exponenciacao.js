0c75c36028b11dcd370705a5545ad603b4768ae7
Danilo Silva <danilo.silva@msn.com>
2022-04-03 15:24:02 -0300

    Fundamentos - Operador de Exponenciação

---
+++ 1-Fundamentos/exponenciacao.js
@@ -0,0 +1,14 @@
+// Maneira antiga
+let exp = Math.pow(2, 3); // O primeiro parâmetro é a base e o segundo o expoente.
+console.log(exp);
+
+// ES7
+let expNovo = 2 ** 3; // O primeiro parâmetro é a base e o segundo o expoente.
+console.log(expNovo);
+
+let base = 2;
+base **= 3; // Eleva o valor armazenado na base à terceira potência
+console.log(base);
+
+let expNegativo = 2 ** -3; // Com expoente negativo
+console.log(expNegativo);

f2d2211964fec0ad19b1e9fd94e382d5085bba35
Danilo Silva <danilo.silva@msn.com>
2022-04-11 00:47:51 -0300

    Exercícios

--- 4-ListaExercicios/exer06.js
+++ 4-ListaExercicios/exer06.js
@@ -3,3 +3,17 @@
  * primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
  * retornará o valor da aplicação sob o regime de juros compostos.
  */
+
+function calculaJurosSimples(capitalInicial, taxaJuros, tempoAplicacao) {
+    let juros = capitalInicial * taxaJuros * tempoAplicacao;
+    let montante = capitalInicial + juros;
+    console.log(montante);
+}
+
+function calculaJurosCompostos(capitalInicial, taxaJuros, tempoAplicacao) {
+    montante = capitalInicial * Math.pow(1 + taxaJuros, tempoAplicacao);
+    console.log(montante.toFixed(2));
+}
+
+calculaJurosSimples(1200, 0.02, 14);
+calculaJurosCompostos(620, 0.015, 24);
